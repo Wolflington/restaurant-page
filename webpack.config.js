9dc9f65d6d735f59773f626d63fab78caea79d39
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-11-22 14:50:31 +0800

    Rebuild project

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -13,12 +12,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //       template: './src/index.html',
-  //   }),
-  // ],
 
   module: {
     rules: [
